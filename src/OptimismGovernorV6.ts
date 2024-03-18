56a5fd2ed4d76bd93181fa51f5139a3f5042f1f6
ncale <nicholasbrodeur@outlook.com>
2024-03-18 22:35:51 +0000

    index vote events

--- src/OptimismGovernorV6.ts
+++ src/OptimismGovernorV6.ts
@@ -1,17 +1,55 @@
 import { ponder } from "@/generated";
 
-ponder.on("OptimismGovernorV6:AdminChanged", async ({ event, context }) => {
-  console.log(event.args);
+ponder.on("OptimismGovernorV6:VoteCast", async ({ event, context }) => {
+	console.log("event: VoteCast", event.args.voter, event.args.proposalId)
+	const { Delegate, Vote } = context.db
+	await Delegate.upsert({
+		id: event.args.voter,
+		create: {
+			address: event.args.voter
+		},
+		update: {
+			address: event.args.voter
+		},
+	})
+	await Vote.upsert({
+		id: event.transaction.hash,
+		create: { 
+			delegateId: event.args.voter,
+			proposalId: event.args.proposalId,
+			blockNum: event.transaction.blockNumber,
+		},
+		update: { 
+			delegateId: event.args.voter,
+			proposalId: event.args.proposalId,
+			blockNum: event.transaction.blockNumber,
+		},
+	})
 });
 
-ponder.on("OptimismGovernorV6:BeaconUpgraded", async ({ event, context }) => {
-  console.log(event.args);
-});
-
-ponder.on("OptimismGovernorV6:Upgraded", async ({ event, context }) => {
-  console.log(event.args);
-});
-
-ponder.on("OptimismGovernorV6:Initialized", async ({ event, context }) => {
-  console.log(event.args);
+ponder.on("OptimismGovernorV6:VoteCastWithParams", async ({ event, context }) => {
+	console.log("event: VoteCast", event.args.voter, event.args.proposalId)
+	const { Delegate, Vote } = context.db
+	await Delegate.upsert({
+		id: event.args.voter,
+		create: {
+			address: event.args.voter
+		},
+		update: {
+			address: event.args.voter
+		},
+	})
+	await Vote.upsert({
+		id: event.transaction.hash,
+		create: { 
+			delegateId: event.args.voter,
+			proposalId: event.args.proposalId,
+			blockNum: event.transaction.blockNumber,
+		},
+		update: { 
+			delegateId: event.args.voter,
+			proposalId: event.args.proposalId,
+			blockNum: event.transaction.blockNumber,
+		},
+	})
 });
