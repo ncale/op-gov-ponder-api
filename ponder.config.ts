d1f4559a67f88de5fd611b4d4c587f60a3a5dc10
ncale <nicholasbrodeur@outlook.com>
2024-03-25 19:32:25 -0500

    remove ens contract info

--- ponder.config.ts
+++ ponder.config.ts
@@ -8,12 +8,10 @@ import { OptimismGovernorV4_0x8518Abi } from "./abis/OptimismGovernorV4_0x8518Ab
 import { OptimismGovernorV5_0x160bAbi } from "./abis/OptimismGovernorV5_0x160bAbi";
 import { OptimismGovernorV6_0x0c01Abi } from "./abis/OptimismGovernorV6_0x0c01Abi";
 import { GovernanceTokenAbi } from "./abis/GovernanceTokenAbi";
-import { ReverseRegistrarAbi } from "./abis/ReverseRegistrarAbi"
 
 export default createConfig({
   networks: {
-    mainnet: { chainId: 1, transport: http(process.env.PONDER_RPC_URL_1) },
-		optimism: { chainId: 10, transport: http(process.env.PONDER_RPC_URL_10) },
+    optimism: { chainId: 10, transport: http(process.env.PONDER_RPC_URL_10) },
   },
   contracts: {
     OptimismGovernorV6: {
@@ -29,17 +27,11 @@ export default createConfig({
       network: "optimism",
       startBlock: 112131709,
     },
-		GovernanceToken: {
-			abi: GovernanceTokenAbi,
-			address: "0x4200000000000000000000000000000000000042",
-			network: "optimism",
-			startBlock: 1, // CHANGE START BLOCK
-		},
-		ReverseRegistrar: {
-			abi: ReverseRegistrarAbi,
-			address: "0xa58E81fe9b61B5c3fE2AFD33CF304c454AbFc7Cb",
-			network: "mainnet",
-			startBlock: 1,
-		},
+    GovernanceToken: {
+      abi: GovernanceTokenAbi,
+      address: "0x4200000000000000000000000000000000000042",
+      network: "optimism",
+      startBlock: 1, // CHANGE START BLOCK
+    },
   },
 });
