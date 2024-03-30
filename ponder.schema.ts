291c2acb5aac633a350fd51d32e1a574565ddccb
Nick Brodeur <106794061+ncale@users.noreply.github.com>
2024-03-30 15:42:48 -0400

    remove bugged contract code (#2)

--- ponder.schema.ts
+++ ponder.schema.ts
@@ -4,7 +4,6 @@ export default createSchema((p) => ({
   Delegate: p.createTable({
     id: p.string(),
     address: p.string(),
-    votingPower: p.bigint(),
     votes: p.many("Vote.delegateId"),
   }),
   Vote: p.createTable({
--- src/OptimismGovernorV6.ts
+++ src/OptimismGovernorV6.ts
@@ -1,34 +1,17 @@
 import { ponder } from "@/generated";
 
-// Create a delegate on DelegateVotesChanged, if one doesn't exist
-ponder.on(
-  "GovernanceToken:DelegateVotesChanged",
-  async ({ event, context }) => {
-    console.log(
-      "event: DelegateVotesChanged",
-      event.args.delegate,
-      "\nnew balance:",
-      event.args.newBalance
-    );
-
-    const { Delegate } = context.db;
-    await Delegate.upsert({
-      id: event.args.delegate,
-      create: {
-        address: event.args.delegate,
-        votingPower: event.args.newBalance,
-      },
-      update: {
-        votingPower: event.args.newBalance,
-      },
-    });
-  }
-);
-
-// Create a vote on VoteCast or VoteCastWithParams
 ponder.on("OptimismGovernorV6:VoteCast", async ({ event, context }) => {
   console.log("event: VoteCast", event.args.voter, event.args.proposalId);
-  const { Vote } = context.db;
+  const { Delegate, Vote } = context.db;
+  await Delegate.upsert({
+    id: event.args.voter,
+    create: {
+      address: event.args.voter,
+    },
+    update: {
+      address: event.args.voter,
+    },
+  });
   await Vote.upsert({
     id: event.transaction.hash,
     create: {
@@ -43,11 +26,21 @@ ponder.on("OptimismGovernorV6:VoteCast", async ({ event, context }) => {
     },
   });
 });
+
 ponder.on(
   "OptimismGovernorV6:VoteCastWithParams",
   async ({ event, context }) => {
     console.log("event: VoteCast", event.args.voter, event.args.proposalId);
-    const { Vote } = context.db;
+    const { Delegate, Vote } = context.db;
+    await Delegate.upsert({
+      id: event.args.voter,
+      create: {
+        address: event.args.voter,
+      },
+      update: {
+        address: event.args.voter,
+      },
+    });
     await Vote.upsert({
       id: event.transaction.hash,
       create: {
